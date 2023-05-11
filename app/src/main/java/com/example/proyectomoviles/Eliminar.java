e56cc4754e27e7178d0faf913b30292492360ed2
juangf3312 <124845241+juangf3312@users.noreply.github.com>
2023-05-10 23:31:35 -0500

    APP

---
+++ app/src/main/java/com/example/proyectomoviles/Eliminar.java
@@ -0,0 +1,16 @@
+package com.example.proyectomoviles;
+
+import static com.example.proyectomoviles.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Eliminar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.eliminar);
+    }
+}
---
+++ app/src/main/java/com/example/proyectomoviles/Promociones.java
@@ -0,0 +1,14 @@
+package com.example.proyectomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Promociones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.promociones);
+    }
+}
